c845a9e977b7dbaf4f1b9db771b2ce42f2d4665a
pravahanj <79738603+pravahanj@users.noreply.github.com>
2021-11-04 11:34:01 +0530

    Update Triplet Sum.cpp

--- Time and Space Complexity/Triplet Sum.cpp
+++ Time and Space Complexity/Triplet Sum.cpp
@@ -54,38 +54,38 @@ For the second query, we have 5 triplets in total that sum up to 10. They are, (
 
 int pairSum(int *arr, int start, int end, int num) {
     int count = 0;
-	for(; start < end;) {
-         	if(arr[start] + arr[end] < num) {
-            	start++;
-        	} else if (arr[start] + arr[end] > num) {
-            	end--;
-        	} else {
-            	int start_element = arr[start];
-            	int end_element =  arr[end];
+    for(; start < end;) {
+         if(arr[start] + arr[end] < num) {
+            start++;
+        } else if (arr[start] + arr[end] > num) {
+            end--;
+        } else {
+            int start_element = arr[start];
+            int end_element =  arr[end];
             
-            	//if all the elements are same
-            	if(start_element == end_element) {
-                	int total = (end - start) + 1;
-                	count += ((total ) * (total - 1) ) / 2;  // n choose 2;
-                	return count;
-            	}
+            //if all the elements are same
+            if(start_element == end_element) {
+                int total = (end - start) + 1;
+                count += ((total ) * (total - 1) ) / 2;  // n choose 2;
+                return count;
+            }
             
-            	int start_temp = start;
-            	int end_temp = end;
+            int start_temp = start;
+            int end_temp = end;
             
-            	for(;start_temp <= end_temp and arr[start_temp] == start_element;) {
-                start_temp++;
-            	}
+            for(;start_temp <= end_temp and arr[start_temp] == start_element;) {
+               	start_temp++;
+            }
             
-            	for(;start_temp <= end_temp and arr[end_temp] == end_element;) {
+            for(;start_temp <= end_temp and arr[end_temp] == end_element;) {
                 end_temp--;
-            	}
+            }
             
-           		count += (start_temp - start) * (end - end_temp);
-        		start = start_temp;
-            	end = end_temp;
-        	}
-    	}
+            count += (start_temp - start) * (end - end_temp);
+            start = start_temp;
+            end = end_temp;
+        }
+    }
     
     return count;
 }
