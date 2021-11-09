17e63aecaf0bb1f16e46a8641e856952740b476e
Pravahan Jaivili <79738603+pravahanj@users.noreply.github.com>
2021-11-09 21:14:46 +0530

    Update Reverse linked list(iterative).cpp

--- Linked List 2/Reverse linked list(iterative).cpp
+++ Linked List 2/Reverse linked list(iterative).cpp
@@ -37,3 +37,45 @@ Sample Output 2 :
 10 
 50 40 30 20 10 
 */
+
+/****************************************************************
+ 
+    Following is the class structure of the Node class:
+
+        class Node
+        {
+        public:
+	        int data;
+	        Node *next;
+	        Node(int data)
+	        {
+		        this->data = data;
+		        this->next = NULL;
+	        }
+        };
+
+*****************************************************************/
+
+Node *reverseLinkedList(Node *head) {
+    // Write your code here
+	//corner case
+    if(head == NULL or head -> next == NULL) {
+        return head;
+    }
+    
+    Node *curr = head;
+    Node *next = NULL;
+    Node *prev = NULL; 
+    
+    while(curr) {
+        next = curr -> next;
+        curr -> next = prev;
+        
+        prev = curr;
+        curr = next;
+    }
+    return prev;
+}
+
+// Time Complexity : O(n)
+// Auxillary Space : O(1)
