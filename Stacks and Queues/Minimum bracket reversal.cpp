0e49752b94c1256d718493a024ac1006ad79e392
Pravahan Jaivili <79738603+pravahanj@users.noreply.github.com>
2021-11-14 22:50:34 +0530

    Update Minimum bracket reversal.cpp

--- Stacks and Queues/Minimum bracket reversal.cpp
+++ Stacks and Queues/Minimum bracket reversal.cpp
@@ -43,23 +43,23 @@ Sample Output 2:
 
 int countBracketReversals(string input){
 	// Write your code here
-  stack<char> st;
+    stack<char> st;
 	int l = input.size();
 	int count;
 	if(!(l & 1)) {
 		for(int i = 0; i < l; i++) {
 			if(input[i] == '{') {
-         //push onto stack
+                //push onto stack
 				st.push(input[i]);
                 
-       } else if(input[i] == '}' and !st.empty()) {
-				  if(st.top() == '{') {
-			       st.pop();
-           } else {
-					   st.push(input[i]);
-           }
-		   } else {
-            st.push(input[i]); // when the first character is '}'
+            } else if(input[i] == '}' and !st.empty()) {
+				if(st.top() == '{') {
+			       	st.pop();
+                } else {
+					st.push(input[i]);
+            	}
+		    } else {
+                st.push(input[i]);
             }
 		}
 		count = 0;
@@ -68,18 +68,19 @@ int countBracketReversals(string input){
 			char c1 = st.top();
 			st.pop();
 			
-      char c2 = st.top();
+            char c2 = st.top();
 			st.pop();
 			
-      if(c1 == c2) {
-				  count++; // if the characters are same we need to invert one of them
-       } else {    //	if they are different we need to invert both
-				 count += 2;
-       }
-		 }
-       return count;
-	} else { 
-    // if the length is odd then we cannot balance them
-		return -1;
+            if(c1 == c2) {
+				count++; // if the characters are same we need to invert one of them
+            } else {    //	if they are different we need to invert both
+				count += 2;
+            }
+		}
+		
+        return count;
+	} else { // if the length is odd then we cannot balance them
+		
+        return -1;
 	}
 }
