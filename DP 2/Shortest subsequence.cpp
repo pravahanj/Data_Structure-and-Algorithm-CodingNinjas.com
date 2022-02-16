cbe303fdd939261529ddbd5304767b28d2c7389a
Pravahan Jaivili <79738603+pravahanj@users.noreply.github.com>
2022-02-16 14:59:45 +0530

    Update Shortest subsequence.cpp

--- DP 2/Shortest subsequence.cpp
+++ DP 2/Shortest subsequence.cpp
@@ -55,7 +55,7 @@ int solve(string s, string v) {
              auto it = v.substr(0,j).rfind(current);
              
              if(it != ULONG_MAX) {
-             	 				// not including the current
+             	 			 // not including the current
                  dp[i][j] = min(dp[i - 1][j], dp[i - 1][it] + 1);
                      						 // including current  
              } else {
